025004b4d93eb842ebfb27b3f6bdd3df66e4b7aa
Richard Yuh <richardyuh1@gmail.com>
2024-11-04 22:15:58 -0800

    save feature

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # In-memory storage for events
-events = []
+events = {}
+
+page_id = "saved"
 
 @app.route('/')
 def index():
-    return render_template('index.html', events=events)
+    return render_template('calendar.html', events=events)
+
+@app.route('/{}'.format(page_id))
+def saved():
+    print(events)
+    return render_template('calendar.html', events=events)
 
 @app.route('/add_events', methods=['POST'])
 def add_events():
-    event = request.form.get('event')
-    events.append(event)
-    return redirect(url_for('index'))
+    global events
+    received = request.form.to_dict(flat=True)  # Converts form data to a dictionary
+    print("Received events:", received)  # For debugging
+    for k,v in received.items():
+        events[int(k)] = v
+    return redirect(url_for('saved'))
 
 if __name__ == '__main__':
     app.run(debug=True)
